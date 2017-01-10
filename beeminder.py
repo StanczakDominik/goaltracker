95dd0c6fc7a7e7a6cea1851e086e2144519f6077
Dominik Stańczak <stanczakdominik@gmail.com>
2017-01-10 13:30:54 +0100

    Saving and loading data, cumulative sum working Neat plot

--- beeminder.py
+++ beeminder.py
@@ -6,11 +6,15 @@ import datetime
 
 goals = {"pushups", "meditation", "duolingo"}
 class Goal:
-    """Class representing a goal you want to pursue."""
+    """Class representing a goal you want to pursue.
+
+    Goal("pushups", "20 pushups first day in morning", 20, 1)
+    shortname - short, no spaces, name for goal
+    description - more verbose description
+    count - how many you want to do...
+    period - per how many days
+    """
     def __init__(self, shortname, description, count, period):
-        """
-        Goal("pushups", "20 pushups first day in morning", 20, "1 day")
-        """
         # TODO: readable format for period?
         self.shortname = shortname
         self.description = description
@@ -19,26 +23,54 @@ class Goal:
         self.df = self.load_df()
 
     def load_df(self):
+        """Loads goal dataframe from csv file.
+        If it doesn't exist, creates a new dataframe."""
+
         filename = self.shortname + ".csv"
         if os.path.isfile(filename):
-            return pd.read_csv(filename)
+            print("Found {}!".format(filename))
+            loaded = pd.read_csv(filename, parse_dates=['datetime'], index_col=0)
+            return loaded
         else:
-            df = pd.DataFrame(columns=['datetime','count'])
+            print("Starting a-fresh in {}!".format(filename))
+            df = pd.DataFrame(columns=['datetime','count'], )
             return df
 
-    def append_df(self, value):
+    def progress(self, value):
+        """Appends occurence of goal with current time to the dataframe's end."""
         current_datetime = datetime.datetime.today()
         self.df.loc[len(self.df)] = [current_datetime, value]
-
+        g.save_df()
 
     def plot_cumsum(self):
+        """Plots a neat comparison of your progress, compared to what you wanted
+        to accomplish in that area."""
+
         y = self.df['count'].cumsum()
         x = self.df['datetime']
-        plt.plot(x,y)
+        plt.plot(x,y, "bo--", label="Your progress!")
+
+        first_day = self.df['datetime'][0].to_pydatetime()
+
+        all_days = (datetime.datetime.today()-first_day).days
+        x_plot = pd.date_range(start=first_day, end=datetime.datetime.today()+datetime.timedelta(1), freq=str(self.period)+'D')
+        y_supposed = np.arange(x_plot.size) * self.count / self.period + self.count
+
+        plt.plot(x_plot, y_supposed, "r-", label="How you should be doing!")
+        plt.vlines([datetime.datetime.today()], y_supposed.min(), y_supposed.max(), "k", "--", label="Right now!")
+        plt.legend(loc='best')
+        plt.grid()
+        plt.xlabel("")
         plt.show()
+
+    def save_df(self):
+        """Saves the dataframe to .csv."""
+        self.df.to_csv(self.shortname + ".csv")
 if __name__=="__main__":
-    g = Goal("pushups", "20 pushups first day in morning", 20, "1 day")
-    for i in range(10):
-        g.append_df(i)
-    print(g.df)
+    g = Goal("pushups", "20 pushups first day in morning", 20, 1)
+    # print(g.df)
+    # for i in range(10):
+    #     g.progress(i)
+    # # g.df.set_value(0,'datetime', datetime.datetime(2017,1,1))
+    # # g.save_df()
     g.plot_cumsum()
